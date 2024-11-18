ab8f20012017d55bebde897f7af49822f946a15e
vinnietito <vincentnjuguna69@gmail.com>
2024-11-18 22:16:45 +0300

    Commits

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="list"),
-    path('update_task/<str:pk>', views.update_task, name="update_task"),
+    path('update_task/<str:pk>/', views.update_task, name="update_task"),
+
 ]
